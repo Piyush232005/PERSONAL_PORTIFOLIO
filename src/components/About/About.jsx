dbfd474674b938e6a1b2de449f14da9e07397e26
Piyush <166606245+Piyush232005@users.noreply.github.com>
2025-06-14 19:46:53 +0530

    first commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import ReactTypingEffect from 'react-typing-effect';
+import Tilt from 'react-parallax-tilt';
+import profileImage from '../../assets/my.png';
+
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
+    >
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+        {/* Left Side */}
+        <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
+          {/* Greeting */}
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-2 leading-tight">
+            Hi, I am
+          </h1>
+          {/* Name */}
+          <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
+            Piyush
+          </h2>
+          {/* Skills Heading with Typing Effect */}
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#456cec] leading-tight">
+            <span className="text-white">I am a </span>
+            <ReactTypingEffect
+              text={[
+                'Fullstack Developer',
+                'Web App Developer',
+                'UI/UX Designer',
+                'Coder',
+              ]}
+              speed={100}
+              eraseSpeed={50}
+              typingDelay={500}
+              eraseDelay={2000}
+              cursorRenderer={(cursor) => (
+                <span className="text-[#456cec]">{cursor}</span>
+              )}
+            />
+          </h3>
+          {/* About Me Paragraph */}
+          <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
+            I am a full-stack developer with Gentle experience in
+            building scalable web applications. Skilled in both front-end and
+            back-end development, I specialize in the MERN stack and other
+            modern technologies to create seamless user experiences and
+            efficient solutions.
+          </p>
+          {/* Resume Button */}
+          <a
+            href="https://drive.google.com/file/d/1wHYPqwhQ8DMVYYa2rcUw_uFwQP2Io8K1/view"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            style={{
+              background: 'linear-gradient(90deg, #456cec, #a855f7)',
+              boxShadow: '0 0 2px #456cec, 0 0 2px #456cec, 0 0 40px #456cec',
+            }}
+          >
+            DOWNLOAD CV
+          </a>
+          
+        </div>
+        {/* Right Side */}
+        <div className="md:w-1/2 flex justify-center md:justify-end">
+          <Tilt
+            className="w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-blue-700 rounded-full"
+            tiltMaxAngleX={20}
+            tiltMaxAngleY={20}
+            perspective={1000}
+            scale={1.05}
+            transitionSpeed={1000}
+            gyroscope={true}
+          >
+            <img
+              src={profileImage}
+              alt="Piyush"
+              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+            />
+          </Tilt>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
